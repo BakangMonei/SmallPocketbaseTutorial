487d04b686fdb8147edb33ee31f04616069d8ebd
BakangMonei <bakangmonei2@gmail.com>
2023-10-21 14:16:30 +0200

    Added the file upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PocketBase from 'pocketbase';
 
 function App() {
-  // Move the PocketBase instance creation to the async function
   const pb = new PocketBase('http://127.0.0.1:8090');
+  const [uploadedFileLink, setUploadedFileLink] = useState('');
 
   const handleUpload = async () => {
     const fileInput = document.getElementById('file');
@@ -19,7 +19,20 @@ function App() {
 
     try {
       const createdRecord = await pb.collection('testfile').create(formData);
+      const fileId = createdRecord.id; // Assuming the ID is used in the URL
+
+      // Generate the link to the uploaded file
+      const fileLink = `http://127.0.0.1:8090/${fileId}`;
+      setUploadedFileLink(fileLink); // Update the state to display in the UI
       console.log('Record created successfully:', createdRecord);
+      console.log('Uploaded file link:', fileLink);
+
+      // Update the record with the fileUrl
+      const updatedRecord = await pb.collection('testfile').update(createdRecord.id, {
+        fileUrl: fileLink,
+      });
+
+      console.log('Record updated with fileUrl:', updatedRecord);
     } catch (error) {
       console.error('Error creating record:', error);
     }
@@ -31,6 +44,14 @@ function App() {
       <button className='bg-red-500 border rounded-md text-black' onClick={handleUpload}>
         Upload
       </button>
+      {uploadedFileLink && (
+        <div>
+          <p>Uploaded File Link:</p>
+          <a href={uploadedFileLink} target="_blank" rel="noopener noreferrer">
+            {uploadedFileLink}
+          </a>
+        </div>
+      )}
     </div>
   );
 }
